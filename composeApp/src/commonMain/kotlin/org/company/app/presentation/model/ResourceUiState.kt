5e6d500e6a9434e8ce3c85c358fe2befab2ff02b
natashalaurie <zakrevskns@gmail.com>
2024-09-20 16:39:32 +0300

    presentation/ setup mvi+viewmodel base classes

---
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/model/ResourceUiState.kt
@@ -0,0 +1,9 @@
+package org.company.app.presentation.model
+
+sealed interface ResourceUiState<out T> {
+    data class Success<T>(val data: T) : ResourceUiState<T>
+    data class Error(val message: String? = null) : ResourceUiState<Nothing>
+    data object Loading : ResourceUiState<Nothing>
+    data object Empty : ResourceUiState<Nothing>
+    data object Idle : ResourceUiState<Nothing>
+}
---
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/mvi/UiState.kt
@@ -0,0 +1,3 @@
+package org.company.app.presentation.mvi
+
+interface UiState
